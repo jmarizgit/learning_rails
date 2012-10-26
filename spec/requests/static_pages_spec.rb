b3d6b9b4ebd747731a54cb09a09cf93263be961c
Mariz Melo <m@emoriz.com>
2012-10-26 15:38:16 -0700

    make basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,7 @@ describe "StaticPages" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get '/'
       response.status.should be(200)
-    end
+    end 
   end
 
   subject { page }
